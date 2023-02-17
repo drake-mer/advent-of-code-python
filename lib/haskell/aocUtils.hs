ba91e6a647378656a03829dfcccedf91153b0171
David Kremer <courrier@david-kremer.fr>
2023-02-17 19:06:40 +0100

    wip

---
+++ lib/haskell/aocUtils.hs
@@ -0,0 +1,14 @@
+module AocUtils where
+
+
+readInput' :: String -> IO [String]
+readInput' fname = do
+  content <- readFile fname
+  let result = lines content
+  return result
+
+
+readInput :: IO [String]
+readInput = readInput' "input.txt"
+
+
