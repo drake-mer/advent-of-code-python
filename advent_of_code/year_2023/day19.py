3bacd16d57cb326eeaa629718e10ff858d74156f
David Kremer <courrier@david-kremer.fr>
2023-12-19 10:14:28 +0100

    eval tree

--- advent_of_code/year_2023/day19.py
+++ advent_of_code/year_2023/day19.py
@@ -1,7 +1,24 @@
 import dataclasses
-from typing import Literal
+from enum import Enum
+from typing import Literal, TypeAlias, Union
 
 from advent_of_code.solution import Solution
+
+
+class Label(str):
+    pass
+
+
+class Action(str, Enum):
+    Accepted = "A"
+    Rejected = "R"
+
+
+@dataclasses.dataclass
+class EvalTree:
+    condition: Union["CompOp", bool] = None
+    falsy: Union["EvalTree", Action] = None
+    truthy: Union["EvalTree", Action] = None
 
 
 @dataclasses.dataclass
@@ -11,7 +28,7 @@ class CompOp:
     value: int
     action: str
 
-    def result(self, part: "Part"):
+    def true(self, part: "Part"):
         match self.operator:
             case ">":
                 return getattr(part, self.variable) > self.value
@@ -50,8 +67,15 @@ class Part:
 @dataclasses.dataclass
 class Workflow:
     label: str
-    condition: list[CompOp]
+    conditions: list[CompOp]
     fallback: str
+
+    def process(self, part):
+        for cond in self.conditions:
+            if cond.true(part):
+                return cond.action
+        else:
+            return self.fallback
 
     @classmethod
     def parse(cls, line):
@@ -64,8 +88,49 @@ class Workflow:
 
 @dataclasses.dataclass
 class Game:
-    workflows: list[Workflow]
+    workflows: dict[Label, Workflow]
     parts: list[Part]
+
+    def process_part(self, part: Part):
+        label = Label("in")
+        flow = self.workflows[label]
+        while (label := flow.process(part)) not in ("A", "R"):
+            flow = self.workflows[label]
+        if label == "A":
+            return part.a + part.s + part.m + part.x
+        elif label == "R":
+            return 0
+        else:
+            raise ValueError()
+
+    def sort_parts(self):
+        acc_parts = 0
+        for part in self.parts:
+            acc_parts += self.process_part(part)
+        return acc_parts
+
+    def eval_tree(self, label: Label = None, other: list[CompOp] = None, fallback: Label = None):
+        if label == "A" or label == "R":
+            return Action(label)
+        if other is None and fallback is None:
+            # initialized with just a label
+            workflow = self.workflows[label]
+            return self.eval_tree(label, workflow.conditions, workflow.fallback)
+        elif not other:
+            # we must fall back
+            if fallback == "A" or fallback == "R":
+                return Action(fallback)
+            next_node = self.workflows[fallback]
+            return self.eval_tree(
+                label=Label(next_node.label), other=next_node.conditions, fallback=Label(next_node.fallback)
+            )
+        else:
+            cond, *other = other
+            return EvalTree(
+                condition=cond,
+                truthy=self.eval_tree(label=Label(cond.action)),
+                falsy=self.eval_tree(other=other, fallback=fallback),
+            )
 
 
 class Day19(Solution):
@@ -77,14 +142,15 @@ class Day19(Solution):
             workflows.append(Workflow.parse(line.strip()))
         for line in data2.splitlines():
             parts.append(Part.parse(line.strip()))
-        return Game(workflows, parts)
+        return Game({w.label: w for w in workflows}, parts)
 
     def solution1(self):
-        print(*self.parsed.parts, sep="\n")
-        print(*self.parsed.workflows, sep="\n")
+        return self.parsed.sort_parts()
 
     def solution2(self):
-        return "not implemented"
+        game: Game = self.parsed
+        all_ways = game.eval_tree(Label("in"))
+        print(all_ways)
 
 
 class Day19Test(Day19):
