4a0e5744b56dd294c3a27799a8bfe57a89f3da91
David Kremer <dkr@sahar.fr>
2024-01-08 10:17:54 +0100

    2023, day20 stub

---
+++ advent_of_code/year_2023/day20.py
@@ -0,0 +1,18 @@
+from advent_of_code.solution import Solution
+
+
+class Day20(Solution):
+    def parse(self):
+        raise NotImplementedError()
+
+    def solution1(self):
+        return "not implemented"
+
+    def solution2(self):
+        return "not implemented"
+
+
+class Day20Test(Day20):
+    @property
+    def data(self):
+        return """"""
