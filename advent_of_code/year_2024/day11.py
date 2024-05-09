9953d109c297ed5263331b0bff4c3a01134b8c49
David Kremer <courrier@david-kremer.fr>
2024-05-09 03:19:42 +0200

    year 2024

---
+++ advent_of_code/year_2024/day11.py
@@ -0,0 +1,18 @@
+from advent_of_code.solution import Solution
+
+
+class Day11(Solution):
+    def parse(self):
+        raise NotImplementedError()
+
+    def solution1(self):
+        return "not implemented"
+
+    def solution2(self):
+        return "not implemented"
+
+
+class Day11Test(Day11):
+    @property
+    def data(self):
+        return """"""
---
+++ advent_of_code/year_2024/day13.py
@@ -0,0 +1,18 @@
+from advent_of_code.solution import Solution
+
+
+class Day13(Solution):
+    def parse(self):
+        raise NotImplementedError()
+
+    def solution1(self):
+        return "not implemented"
+
+    def solution2(self):
+        return "not implemented"
+
+
+class Day13Test(Day13):
+    @property
+    def data(self):
+        return """"""
