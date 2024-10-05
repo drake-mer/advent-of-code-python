1707bbd013a1e39fe7d9729dc46efb2d7d60de07
David Kremer <courrier@david-kremer.fr>
2024-10-05 19:33:26 +0200

    day7, day5, year 2019

--- advent_of_code/year_2019/day05.py
+++ advent_of_code/year_2019/day05.py
@@ -6,7 +6,7 @@ from advent_of_code.solution import Solution
 class OpCode(int, enum.Enum):
     ADD = 1
     MULTIPLY = 2
-    STORE = 3
+    INPUT = 3
     OUTPUT = 4
     JUMP_IF_TRUE = 5
     JUMP_IF_FALSE = 6
@@ -26,9 +26,11 @@ class ReturnCode(int, enum.Enum):
 
 
 class ComputerMemory(list):
-    def __init__(self, instructions: list[str]):
+    def __init__(self, instructions: list[str], input_buffer=None):
         super().__init__(instructions)
-        self.output_buffer = None
+        self.output_buffer = list()
+        self.input_buffer = input_buffer
+        self.position = 0
 
     def get_value(self, arg: str | int, mode) -> int:
         match mode:
@@ -38,83 +40,79 @@ class ComputerMemory(list):
                 return int(arg)
         raise ValueError("incorrect state in the calling function")
 
-    def store_input(self, arg_1: str | int):
+    def store_input(self, arg_1: str | int = None):
         arg_1 = int(arg_1, 10) if isinstance(arg_1, str) else arg_1
-        self[arg_1] = self.output_buffer
-        self.output_buffer = None
+        if not self.input_buffer:
+            val = int(input("please enter a number"))
+        else:
+            val, *remaining = self.input_buffer
+            self.input_buffer = remaining
+        self[arg_1] = val
 
     def output_value(self, value):
-        self.output_buffer = value
-        print("output value:", self.output_buffer)
+        self.output_buffer.append(value)
 
+    def run_program(self):
+        element = self[self.position]
+        remains = self[self.position + 1 :]
+        element = str(element)  # convert back to string
+        operation = OpCode(int(element[-2:], 10))
+        mode_a = mode_b = mode_c = Mode.PARAM
+        if len(element[:-2]) == 1:
+            mode_c = element[0]  # this is default mode
+        elif len(element[:-2]) == 2:
+            mode_b, mode_c = element[:-2]
+        elif len(element[:-2]) == 3:
+            mode_a, mode_b, mode_c = element[:-2]
+        match operation:
+            case OpCode.HALT:
+                return self.output_buffer
+            case OpCode.MULTIPLY | OpCode.ADD | OpCode.LESS_THAN | OpCode.EQUALS:
+                arg_1, arg_2, arg_3, *remains = remains
+                assert mode_a == Mode.PARAM
+                arg_1 = self.get_value(arg_1, mode=Mode(mode_c))
+                arg_2 = self.get_value(arg_2, mode=Mode(mode_b))
+                arg_3 = int(arg_3)  # should always be mode PARAM
+                match operation:
+                    case OpCode.MULTIPLY:
+                        self[arg_3] = arg_1 * arg_2
+                    case OpCode.ADD:
+                        self[arg_3] = arg_1 + arg_2
+                    case OpCode.LESS_THAN:
+                        if arg_1 < arg_2:
+                            self[arg_3] = 1
+                        else:
+                            self[arg_3] = 0
+                    case OpCode.EQUALS:
+                        if arg_1 == arg_2:
+                            self[arg_3] = 1
+                        else:
+                            self[arg_3] = 0
+                self.position += 4
+            case OpCode.INPUT | OpCode.OUTPUT:
+                arg_1, *remains = remains
+                match operation:
+                    case OpCode.INPUT:
+                        assert mode_c == Mode.PARAM
+                        self.store_input(arg_1)
+                    case OpCode.OUTPUT:
+                        value = self.get_value(arg_1, mode=Mode(mode_c))
+                        self.output_value(value)
+                self.position += 2
+            case OpCode.JUMP_IF_TRUE | OpCode.JUMP_IF_FALSE:
+                arg_1, arg_2, *remains = remains
+                arg_1 = self.get_value(arg_1, mode=Mode(mode_c))
+                arg_2 = self.get_value(arg_2, mode=Mode(mode_b))
+                if arg_1 != 0 and operation == OpCode.JUMP_IF_TRUE:
+                    self.position = arg_2
+                elif arg_1 == 0 and operation == OpCode.JUMP_IF_FALSE:
+                    self.position = arg_2
+                else:
+                    self.position += 3
 
-def compute_solution(
-    memory: ComputerMemory,
-    position=0,
-    start=None,
-):
-    if start is not None:
-        memory.output_value(start)
-    element = memory[position]
-    remains = memory[position + 1 :]
-    element = str(element)  # convert back to string
-    operation = OpCode(int(element[-2:], 10))
-    mode_a = mode_b = mode_c = Mode.PARAM
-    if len(element[:-2]) == 1:
-        mode_c = element[0]  # this is default mode
-    elif len(element[:-2]) == 2:
-        mode_b, mode_c = element[:-2]
-    elif len(element[:-2]) == 3:
-        mode_a, mode_b, mode_c = element[:-2]
-    match operation:
-        case OpCode.HALT:
-            return memory.output_buffer
-        case OpCode.MULTIPLY | OpCode.ADD | OpCode.LESS_THAN | OpCode.EQUALS:
-            arg_1, arg_2, arg_3, *remains = remains
-            assert mode_a == Mode.PARAM
-            arg_1 = memory.get_value(arg_1, mode=Mode(mode_c))
-            arg_2 = memory.get_value(arg_2, mode=Mode(mode_b))
-            arg_3 = int(arg_3)  # should always be mode PARAM
-            match operation:
-                case OpCode.MULTIPLY:
-                    memory[arg_3] = arg_1 * arg_2
-                case OpCode.ADD:
-                    memory[arg_3] = arg_1 + arg_2
-                case OpCode.LESS_THAN:
-                    if arg_1 < arg_2:
-                        memory[arg_3] = 1
-                    else:
-                        memory[arg_3] = 0
-                case OpCode.EQUALS:
-                    if arg_1 == arg_2:
-                        memory[arg_3] = 1
-                    else:
-                        memory[arg_3] = 0
-            position += 4
-        case OpCode.STORE | OpCode.OUTPUT:
-            arg_1, *remains = remains
-            match operation:
-                case OpCode.STORE:
-                    assert mode_c == Mode.PARAM
-                    memory.store_input(arg_1)
-                case OpCode.OUTPUT:
-                    value = memory.get_value(arg_1, mode=Mode(mode_c))
-                    memory.output_value(value)
-            position += 2
-        case OpCode.JUMP_IF_TRUE | OpCode.JUMP_IF_FALSE:
-            arg_1, arg_2, *remains = remains
-            arg_1 = memory.get_value(arg_1, mode=Mode(mode_c))
-            arg_2 = memory.get_value(arg_2, mode=Mode(mode_b))
-            if arg_1 != 0 and operation == OpCode.JUMP_IF_TRUE:
-                position = arg_2
-            elif arg_1 == 0 and operation == OpCode.JUMP_IF_FALSE:
-                position = arg_2
-            else:
-                position += 3
-
-        case _:
-            raise ValueError("unsupported case")
-    return compute_solution(memory, position=position)
+            case _:
+                raise ValueError("unsupported case")
+        return self.run_program()
 
 
 class Day05(Solution):
@@ -122,10 +120,9 @@ class Day05(Solution):
         return self.line.split(",")
 
     def solution1(self):
-        # computer_memory = defaultdict(lambda: 0)
-        memory = ComputerMemory(self.parsed)
-        print("final output: ", compute_solution(memory, start=1))
+        memory = ComputerMemory(self.parsed, input_buffer=[1])
+        return memory.run_program()[-1]
 
     def solution2(self):
-        memory = ComputerMemory(self.parsed)
-        print("final output: ", compute_solution(memory, start=5))
+        memory = ComputerMemory(self.parsed, input_buffer=[5])
+        return memory.run_program()[-1]
--- advent_of_code/year_2019/day08.py
+++ advent_of_code/year_2019/day08.py
@@ -1,9 +1,25 @@
+import itertools
+
 from advent_of_code.solution import Solution
+from advent_of_code.year_2019.day05 import ComputerMemory, compute_solution
+
+
+def thruster_output(program: list[str], init_phase: list[int]):
+    signal = 0
+    for k in range(5):
+        amplifier = ComputerMemory(list(program), input_buffer=[init_phase[k], signal])
+        signal = compute_solution(amplifier)[-1]
+    return signal
 
 
 class Day08(Solution):
+    def parse(self):
+        return self.line.split(",")
+
     def solution1(self):
-        return "not implemented"
+        return max(
+            thruster_output(self.parsed, init_phase=list(phase)) for phase in itertools.combinations(range(5), 5)
+        )
 
     def solution2(self):
         return "not implemented"
