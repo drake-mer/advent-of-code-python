c8dde0c04448d1e0c1f274ef70a2a31747d50ffd
David Kremer <courrier@david-kremer.fr>
2024-10-07 23:25:27 +0200

    year 2019, day9

--- advent_of_code/year_2019/day05.py
+++ advent_of_code/year_2019/day05.py
@@ -1,4 +1,5 @@
 import enum
+import logging
 
 from advent_of_code.solution import Solution
 
@@ -12,17 +13,24 @@ class OpCode(int, enum.Enum):
     JUMP_IF_FALSE = 6
     LESS_THAN = 7
     EQUALS = 8
+    INC_BASE = 9
     HALT = 99
 
 
 class Mode(str, enum.Enum):
     PARAM = "0"
     VALUE = "1"
+    RELAT = "2"
 
 
 class ReturnCode(int, enum.Enum):
     OK = True
     STOP = False
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+logger.addHandler(logging.StreamHandler())
 
 
 class ComputerMemory(list):
@@ -31,6 +39,7 @@ class ComputerMemory(list):
         self.output_buffer = list()
         self.input_buffer = input_buffer
         self.position = 0
+        self.relative_base = 0
 
     def get_value(self, arg: str | int, mode) -> int:
         match mode:
@@ -38,6 +47,8 @@ class ComputerMemory(list):
                 return int(self[int(arg)])
             case Mode.VALUE:
                 return int(arg)
+            case Mode.RELAT:
+                return int(self[int(arg) + self.relative_base])
         raise ValueError("incorrect state in the calling function")
 
     def store_input(self, arg_1: str | int = None):
@@ -55,6 +66,11 @@ class ComputerMemory(list):
         self.position += 2
 
     def run_program(self):
+        while int(self[self.position]) != OpCode.HALT:
+            self.run_instruction()
+        return self.output_buffer
+
+    def run_instruction(self):
         element = self[self.position]
         remains = self[self.position + 1 :]
         element = str(element)  # convert back to string
@@ -71,10 +87,27 @@ class ComputerMemory(list):
                 return self.output_buffer
             case OpCode.MULTIPLY | OpCode.ADD | OpCode.LESS_THAN | OpCode.EQUALS:
                 arg_1, arg_2, arg_3, *remains = remains
-                assert mode_a == Mode.PARAM
+                match mode_a:
+                    case Mode.PARAM:
+                        arg_3 = int(arg_3)
+                    case Mode.RELAT:
+                        arg_3 = int(arg_3) + self.relative_base
+                    case _:
+                        raise ValueError(
+                            "this argument must be an absolute or relative address"
+                        )
+
                 arg_1 = self.get_value(arg_1, mode=Mode(mode_c))
                 arg_2 = self.get_value(arg_2, mode=Mode(mode_b))
-                arg_3 = int(arg_3)  # should always be mode PARAM
+
+                logger.debug(
+                    f"position %s, operation %s, arg1=%s, arg2=%s, arg3=%s",
+                    self.position,
+                    OpCode(operation),
+                    arg_1,
+                    arg_2,
+                    arg_3,
+                )
                 match operation:
                     case OpCode.MULTIPLY:
                         self[arg_3] = arg_1 * arg_2
@@ -91,17 +124,41 @@ class ComputerMemory(list):
                         else:
                             self[arg_3] = 0
                 self.position += 4
-            case OpCode.INPUT | OpCode.OUTPUT:
+            case OpCode.INPUT | OpCode.OUTPUT | OpCode.INC_BASE:
                 arg_1, *remains = remains
+                logger.debug(
+                    "position %s, operation %s, arg1=%s",
+                    self.position,
+                    OpCode(operation),
+                    arg_1,
+                )
                 match operation:
                     case OpCode.INPUT:
-                        assert mode_c == Mode.PARAM
-                        self.store_input(arg_1)
+                        match mode_c:
+                            case Mode.PARAM:
+                                self.store_input(int(arg_1))
+                            case Mode.RELAT:
+                                self.store_input(int(arg_1) + self.relative_base)
+                            case _:
+                                raise ValueError(
+                                    "The input parameter must be an address"
+                                )
                     case OpCode.OUTPUT:
                         value = self.get_value(arg_1, mode=Mode(mode_c))
                         self.output_value(value)
+                    case OpCode.INC_BASE:
+                        self.relative_base += self.get_value(arg_1, mode=Mode(mode_c))
+                        self.position += 2
+
             case OpCode.JUMP_IF_TRUE | OpCode.JUMP_IF_FALSE:
                 arg_1, arg_2, *remains = remains
+                logger.debug(
+                    "position %s, operation %s, arg1=%s, arg2=%s",
+                    self.position,
+                    OpCode(operation),
+                    arg_1,
+                    arg_2,
+                )
                 arg_1 = self.get_value(arg_1, mode=Mode(mode_c))
                 arg_2 = self.get_value(arg_2, mode=Mode(mode_b))
                 if arg_1 != 0 and operation == OpCode.JUMP_IF_TRUE:
@@ -113,7 +170,6 @@ class ComputerMemory(list):
 
             case _:
                 raise ValueError("unsupported case")
-        return self.run_program()
 
 
 class Day05(Solution):
--- advent_of_code/year_2019/day09.py
+++ advent_of_code/year_2019/day09.py
@@ -1,9 +1,42 @@
-from advent_of_code.solution import Solution
+from unittest.mock import patch
+
+from advent_of_code.year_2019.day05 import ComputerMemory, Day05
 
 
-class Day09(Solution):
+class Day09(Day05):
     def solution1(self):
-        return "not implemented"
+        return ComputerMemory(self.parsed + [0] * 1000, input_buffer=[1]).run_program()[
+            0
+        ]
 
     def solution2(self):
-        return "not implemented"
+        return ComputerMemory(self.parsed + [0] * 1000, input_buffer=[2]).run_program()[
+            0
+        ]
+
+
+class Day09Test(Day09):
+    @property
+    def line(self):
+        return self.data
+
+    @property
+    def parsed(self):
+        return self.parse()
+
+    def solution1(self):
+        with patch.object(
+            self, "data", "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+        ):
+            original_result = super().solution1()
+            assert ",".join(str(x) for x in original_result) == self.data
+
+        with patch.object(self, "data", "1102,34915192,34915192,7,4,7,99,0"):
+            assert self.data == "1102,34915192,34915192,7,4,7,99,0"
+            original_result = super().solution1()
+            assert len(original_result) == 1
+            assert len(str(original_result[0])) == 16
+
+        with patch.object(self, "data", "104,1125899906842624,99"):
+            original_result = super().solution1()
+            assert original_result == [1125899906842624]
